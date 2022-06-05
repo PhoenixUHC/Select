7a3b1bbc0e7113ee20bdb5100eab623a85a9ce8d
Joseph <joseph300905@gmail.com>
2022-06-05 12:27:03 +0200

    Create getInstance method

--- api/src/main/java/io/github/phoenix/Select.java
+++ api/src/main/java/io/github/phoenix/Select.java
@@ -1,5 +1,11 @@
 package io.github.phoenix;
 
+import org.bukkit.Bukkit;
+
 public interface Select {
     Menu createMenu(String title, int size);
+
+    default Select getInstance() {
+        return (Select) Bukkit.getPluginManager().getPlugin("Select");
+    }
 }

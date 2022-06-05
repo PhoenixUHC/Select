9da5e91dfd32a55925a27bd6f074bc39a07fc40b
Joseph <joseph300905@gmail.com>
2022-06-05 15:33:43 +0200

    Fix getInstance function

--- api/src/main/java/io/github/phoenix/Select.java
+++ api/src/main/java/io/github/phoenix/Select.java
@@ -5,7 +5,7 @@ import org.bukkit.Bukkit;
 public interface Select {
     Menu createMenu(String title, int size);
 
-    default Select getInstance() {
+    static Select getInstance() {
         return (Select) Bukkit.getPluginManager().getPlugin("Select");
     }
 }
